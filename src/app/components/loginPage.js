14c9c273ee2ec19f9a5cc126f662a968d8d090f0
kokocyber <taha.ali.oneplus@gmail.com>
2024-05-16 16:46:06 +0200

    feat: first verision of login form

---
+++ src/app/components/loginPage.js
@@ -0,0 +1,81 @@
+"use client"
+
+import Image from "next/image";
+import { Button, FilledInput, FormControl, Grid, IconButton, InputAdornment, InputLabel, TextField } from "@mui/material";
+import Logo from "../pic/logo.png"
+import { useState } from "react";
+import Link from "next/link";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+    const handleChangeEmail = (event) => {
+        let data = event.target.value;
+        setEmail(data);
+    }
+
+    const handleChangePassword = (event) => {
+        let data = event.target.value;
+        setPassword(data);
+    }
+  return (
+    <Grid container className="border">
+        <Grid item xs={12}>
+            <Image src={Logo} alt="Logo of company" />
+        </Grid>
+        <Grid item xs={12}>
+            <TextField
+            label="Email"
+            value={email}
+            onChange={handleChangeEmail}
+            variant="filled"
+            placeholder="example@domain.com"
+            type="email"
+            required
+            error={!email}
+            />
+        </Grid>
+        <Grid item xs={12}>
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
+          <InputLabel error={!password} required>Password</InputLabel>
+          <FilledInput
+            id="filled-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            required
+            error={!password}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+            <Link href="/vault">
+            <Button
+            variant="contained"
+           >Sign in</Button>
+           </Link>
+            </Grid>
+            
+    </Grid>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import LoginPage from "./components/loginPage";
 
 export default function Home() {
   return (
    <>
-    welcome
+   <LoginPage />
    </>
   );
 }
