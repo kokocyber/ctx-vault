08e652f251d803399bee2a202f89fab7639d519d
kokocyber <taha.ali.oneplus@gmail.com>
2024-05-30 16:54:41 +0200

    feat: added homepage

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useState } from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -10,7 +9,6 @@ import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import {
   FilledInput,
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,35 @@
-import Image from "next/image";
-import LoginPage from "./components/loginPage";
+import { Grid } from "@mui/material";
+import Link from "next/link";
+import Logo from "../../public/pic/logo.png";
 
 export default function Home() {
   return (
     <>
-      <LoginPage />
+      <nav>
+        <Grid container>
+          <Grid item xs={4}>
+            <img src={Logo} alt="Logo" />
+          </Grid>
+          <Grid item container xs={6}>
+            <Grid item>
+              <Link href="/">Home</Link>
+            </Grid>
+            <Grid item>
+              <Link href="/pricing">Pricing</Link>
+            </Grid>
+            <Grid item>
+              <Link href="/about">About</Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </nav>
+      <Grid container justifyContent="center">
+        <Grid item xs={6}>
+          <h1>
+            Guarding Your Digital Fortress, <br /> One Key at a Time.
+          </h1>
+        </Grid>
+      </Grid>
     </>
   );
 }
