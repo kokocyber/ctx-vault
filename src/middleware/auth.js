af7884d10ff0bb58860958dbb2a3370998fb99b6
chris911e <christopherscheel99@gmail.com>
2024-06-05 13:16:10 +0200

    unnecessary hashing

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 import { jwtVerify, SignJWT } from "jose";
+import { sha512 } from "js-sha512";
 
 const secretKey = process.env.SECRET_KEY
 
@@ -15,4 +16,13 @@ export async function decrypt(token) {
         algorithms: ["HS256"]
     })
     return payload
+}
+
+export function securePassword(password) {
+    const hashedPassword = sha512(password)
+    const hashedKey = sha512(secretKey)
+
+    const superSecure = sha512(hashedPassword + hashedKey)
+
+    return hashedPassword + superSecure + hashedKey
 }
