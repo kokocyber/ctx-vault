ff4db9f75b1b468884674930437f49c1001ad57c
chris911e <christopherscheel99@gmail.com>
2024-06-06 20:58:33 +0200

    feat: create password endpoint

--- src/app/api/v1/password/route.js
+++ src/app/api/v1/password/route.js
@@ -3,10 +3,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
+// retrieve all passwords of categoryId
 async function getPasswords(categoryId) {
     const data = prisma.password.findMany({
         where: {
             categoryId: categoryId
+        }
+    })
+    return data
+}
+
+// create password of categoryId
+async function createPassword(password, categoryId) {
+    const data = prisma.password.create({
+        data: {
+            password: password,
+            category: {
+                connect: {
+                    id: categoryId
+                }
+            }
         }
     })
     return data
@@ -35,5 +51,36 @@ export async function GET(request) {
     } catch(e) {
         prisma.$disconnect()
         return Response.json({"error": e}, {status: 404})
+    }
+}
+
+// POST category password of user
+export async function POST(request) {
+    try {
+        const { searchParams } = new URL(request.url)
+        const userId = parseInt(searchParams.get("id"))
+        const categoryId = parseInt(searchParams.get("categoryId"))
+        const password = searchParams.get("password")
+
+        // TODO: Encrypt password
+        const encryptedPassword = password
+
+        const session = await getSession()
+        if(!session) {
+            await prisma.$disconnect()
+            return Response.json({"Unauthorized": "Not logged in!"}, {status: 401})
+        }
+        if(session.user.id !== userId && session.user.role !== "Admin") {
+            await prisma.$disconnect()
+            return Response.json({"Unauthorized": "Not enough permission!"}, {status: 403})
+        }
+        const data = await createPassword(password, categoryId)
+        await prisma.$disconnect()
+        return Response.json({"passwords": data}, {status: 200})
+
+    } catch(e) {
+        prisma.$disconnect()
+        return Response.json({"error": e}, {status: 404})
+
     }
 }
