5be0943e05d0c45241e5c0c1c66e4d3bf28ced19
chris911e <christopherscheel99@gmail.com>
2024-06-11 22:57:38 +0200

    feat: User Context

---
+++ src/app/(context)/UserContextComponent.js
@@ -0,0 +1,39 @@
+"use client";
+
+import { createContext, useEffect, useRef, useState } from "react";
+import { verifyCookie } from "../(util)/api";
+
+export const UserContext = createContext();
+
+async function verifySession() {
+  const response = await verifyCookie()
+  if(response.id) {
+    return response
+  }
+}
+
+export default function UserContextComponent({ children }) {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const userData = useRef(0)
+
+  useEffect(() => {
+    if(document.cookie) {
+      verifySession()
+        .then((response) => {
+          if(response.id) {
+            setIsUserLoggedIn(true)
+            userData.current = response
+          } else {
+            setIsUserLoggedIn(false)
+            userData.current = ""
+          }
+        })
+    }
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ isUserLoggedIn, setIsUserLoggedIn, userData }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
