00ad697f153a4e1d698183ec617bd52ff9c3cf78
chris911e <christopherscheel99@gmail.com>
2024-07-02 09:48:12 +0200

    feat: functional principle high order

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -42,5 +42,26 @@ export function securePassword(password) {
     } catch(e) {
         return e
     }
+}
 
+export function withSession(handler) {
+    return async function(request) {
+        const session = await getSession()
+        if(!session) {
+            return Response.json({ "Unauthorized": "Not logged in"}, { status: 401 })
+        }
+        return handler(request, session)
+    }
+}
+
+export function withPermission(handler) {
+    return async function(request, session) {
+        const { searchParams } = new URL(request.url)
+        const userId = parseInt(searchParams.get("id"))
+
+        if(session.user.id !== userId && session.user.role !== "Admin") {
+            return Response.json({ "Unauthorized": "Not enough permission" }, { status: 403 })
+        }
+        return handler(request, session)
+    }
 }
